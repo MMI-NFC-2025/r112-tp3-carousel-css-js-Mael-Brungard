561c11feafcb26b12c8514f8c57314a90cd25bff
BooMooN <mael.brungard44320@gmail.com>
2025-11-18 16:13:22 +0100

    défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,18 @@
 // *** CAROUSEL ***
+const carousel = document.querySelector('.carousel__container');
+const nextbtn = document.querySelector('.carousel__button--next');
+const prevbtn = document.querySelector('.carousel__button--prev');
+
+const premierItem = document.querySelector(".carousel__item");
+const scrollAmount = premierItem.clientWidth;
+
+if (carousel) {
+    nextbtn.addEventListener('click', () => {
+        carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });
+    });
+    prevbtn.addEventListener('click', () => {
+        carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    });
+}
+
 // TODO
